79c0472815e5b52a8e30c693da6385770d932ff7
HackerMan888 <144242698+HackerMan888@users.noreply.github.com>
2023-10-03 08:42:05 -0400

    Update program.py

--- program.py
+++ program.py
@@ -1,5 +1,5 @@
 # My Python Program
-# Task:  Use the function myFunction to output a simple "Hello World!" statement
+# Task:  Use the function myFunction to output a simple "Hello Charlie!" statement
 
 def myFunction():
   # My Function
